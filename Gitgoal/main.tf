161a75037d88a3a7af0339f374e8988e56238ca6
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2024-08-25 14:12:42 +0530

    add new update in repo

--- Gitgoal/main.tf
+++ Gitgoal/main.tf
@@ -11,3 +11,24 @@ account_replaction_type = "LRS"
 account_tier = "Standard"
 }
 
+resource "azurerm_storage_container" "con" {
+     name = "man-con"
+     storage_account_name = "man-st"
+     container_access_type = "private"
+}
+
+resource "azurerm_virtual_network" "vnetblock" {
+name = "man-vnet"
+location = "westus"
+resource_group_name = "man-rg"
+address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+name = "man-subnet"
+location = "westus"
+resource_group_named= "man-rg"
+virtual_network_name = "man-vnet"
+address_prefix = ["10.0.1.0/24"]
+}
+
