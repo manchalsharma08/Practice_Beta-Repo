0b6ada96c020362541953f4ed6b01eec9d7a2d93
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2024-08-25 13:53:26 +0530

    add storage account by github

--- Gitgoal/main.tf
+++ Gitgoal/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "rgblock" {
   name = "man-rg"
   location = "westus"
 }
+
+resource "azurerm_storage_account" "stblock" {
+name = "man000783st"
+location = "westus"
+resource_group_name = "man-rg"
+account_replaction_type = "LRS"
+account_tier = "Standard"
+}
+
